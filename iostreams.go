9f7202a3f3e5dfae156ec76951c044565c526781
y2k <y.karamani@gmail.com>
2016-03-16 11:14:46 +0300

    check stdin stat

--- iostreams.go
+++ iostreams.go
@@ -10,21 +10,30 @@ func ReadStdin(process func(row []byte) error) error {
 
 	var input []byte = nil
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		bytes, hasMoreInLine, err := reader.ReadLine()
-		if err != nil {
-			if err == io.EOF {
-				return nil
-			}
-			return err
-		}
-		input = append(input, bytes...)
-		if !hasMoreInLine {
-			if err := process(input); err != nil {
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		return err
+	}
+
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			bytes, hasMoreInLine, err := reader.ReadLine()
+			if err != nil {
+				if err == io.EOF {
+					return nil
+				}
 				return err
 			}
-			input = nil
+			input = append(input, bytes...)
+			if !hasMoreInLine {
+				if err := process(input); err != nil {
+					return err
+				}
+				input = nil
+			}
 		}
 	}
+
+	return nil
 }
